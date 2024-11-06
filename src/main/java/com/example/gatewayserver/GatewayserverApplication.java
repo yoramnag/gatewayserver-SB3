3ce11336f6c439c75a0780e9ad7b85ff0dc87bb7
yoram <yoramnag@gmail.com>
2024-11-07 00:30:42 +0200

    add correlationId and call to config server

--- src/main/java/com/example/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,32 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator creditCardRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/creditCard/transactions-service/**")
+						.filters( f -> f.rewritePath("/creditCard/transactions-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://TRANSACTIONS-SERVICE"))
+				.route(p -> p
+						.path("/creditCard/black-list-service/**")
+						.filters( f -> f.rewritePath("/creditCard/black-list-service/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://BLACK-LIST-SERVICE")).build();
 	}
 
 }
